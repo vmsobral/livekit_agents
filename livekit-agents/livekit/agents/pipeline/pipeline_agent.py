4a19b8dfa4654719867b6a4db1b2e4925b68e2ab
Vinicius Sobral <vinicius.sobral@withclutch.com>
2025-02-10 19:17:22 -0300

    adding check for str on before_llm_cb response

--- livekit-agents/livekit/agents/pipeline/pipeline_agent.py
+++ livekit-agents/livekit/agents/pipeline/pipeline_agent.py
@@ -765,8 +765,8 @@ class VoicePipelineAgent(utils.EventEmitter[EventTypes]):
                 handle.cancel()
                 return
 
-            # fallback to default impl if no custom/user stream is returned
-            if not isinstance(llm_stream, LLMStream):
+            # fallback to default impl if no custom/user stream or llm processed str is returned
+            if not isinstance(llm_stream, LLMStream) and not isinstance(llm_stream, str):
                 llm_stream = _default_before_llm_cb(self, chat_ctx=copied_ctx)
 
             if handle.interrupted:
